316442556338f5ee9a7c5729560de0ecb754a630
Denis Tolmachev <oovilden@gmail.com>
2018-10-27 13:23:26 +0500

    Обновляет зависимости

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 import assert from 'assert';
 
 describe('Test', () => {
-    it('should work', () => {
-        assert(true == true);
-    });
+  it('should work', () => {
+    assert(true == true);
+  });
 });
