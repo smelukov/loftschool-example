e733289adc9516e1b2ec8bed76b35824aeb68e3c
Pavel Trofimov <helloloblin@gmail.com>
2019-02-12 19:46:18 +0300

    finish_homework

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ function returnFirstArgument(a) {
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b=100) {
+function sumWithDefaults(a, b = 100) {
   return a + b;
 }
 
@@ -43,10 +43,8 @@ function sumWithDefaults(a, b=100) {
    returnFnResult(() => 'привет') вернет 'привет'
  */
 function returnFnResult(fn) {
-
   return fn();
 }
-
 
 /*
  Задание 4:
@@ -61,45 +59,53 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {
-  console.log("test")
+function returnCounter(number = 0) {
+  return function fn() {
+    return ++number;
+  }
 }
+var f = returnCounter();
+
 
 /*
  Задание 5 *:
-
+ 
  Функция должна возвращать все переданные ей аргументы в виде массива
  Количество переданных аргументов заранее неизвестно
-
+ 
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {
+
+function returnArgumentsArray(...args) {
+  return args;
 }
 
 /*
  Задание 6 *:
-
+ 
  Функция должна принимать другую функцию (F) и некоторое количество дополнительных аргументов
  Функция должна привязать переданные аргументы к функции F и вернуть получившуюся функцию
-
+ 
  Пример:
    function sum(a, b) {
      return a + b;
    }
-
+ 
    var newSum = bindFunction(sum, 2, 4);
-
+ 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn) {
+
+function bindFunction(fn, ...args) {
+  return fn.bind(null, ...args);
 }
 
 export {
-    returnFirstArgument,
-    sumWithDefaults,
-    returnArgumentsArray,
-    returnFnResult,
-    returnCounter,
-    bindFunction
+  returnFirstArgument,
+  sumWithDefaults,
+  returnArgumentsArray,
+  returnFnResult,
+  returnCounter,
+  bindFunction
 }
