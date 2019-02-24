cd9f9951156fded5736fe51f2d271cd0d1bdb1a5
Belov Pavel <belovpavel@protonmail.com>
2019-02-24 16:51:26 +0300

    homework#5

---
+++ src/dnd.js
@@ -0,0 +1,106 @@
+/* Задание со звездочкой */
+
+/*
+ Создайте страницу с кнопкой.
+ При нажатии на кнопку должен создаваться div со случайными размерами, цветом и позицией на экране
+ Необходимо предоставить возможность перетаскивать созданные div при помощи drag and drop
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+ */
+
+/*
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
+
+ Пример:
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
+ */
+const homeworkContainer = document.querySelector('#homework-container');
+
+/*
+ Функция должна создавать и возвращать новый div с классом draggable-div и случайными размерами/цветом/позицией
+ Функция должна только создавать элемент и задвать ему случайные размер/позицию/цвет
+ Функция НЕ должна добавлять элемент на страницу. На страницу элемент добавляется отдельно
+
+ Пример:
+   const newDiv = createDiv();
+   homeworkContainer.appendChild(newDiv);
+ */
+function createDiv() {
+    const div = document.createElement('div');
+
+    function getRandomSize(square) {
+        return Math.floor(Math.random() * square + 80) + 'px';
+    }
+
+    function getRandomColor() {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        return color;
+    }
+
+    div.classList.add('draggable-div');
+    div.style.width = getRandomSize(600);
+    div.style.height = getRandomSize(600);
+    div.style.top = getRandomSize(300);
+    div.style.left = getRandomSize(300);
+    div.style['background-color'] = getRandomColor();
+
+    return div;
+}
+
+/*
+ Функция должна добавлять обработчики событий для перетаскивания элемента при помощи drag and drop
+
+ Пример:
+   const newDiv = createDiv();
+   homeworkContainer.appendChild(newDiv);
+   addListeners(newDiv);
+ */
+function addListeners(target) {
+  
+    let moveElement = false;
+    let deltaX, deltaY;
+
+    target.addEventListener('mousedown', function(e) {
+        e.target.classList.add('clicked');
+        moveElement = true;
+        deltaX = e.clientX - e.target.offsetLeft;
+        deltaY = e.clientY - e.target.offsetTop
+    });
+
+    target.addEventListener('mouseup', function(e) {
+        e.target.classList.remove('clicked');
+        moveElement = false
+    });
+
+    target.addEventListener('mousemove', function(e) {
+        if (moveElement) {
+            e.target.style.top = `${e.clientY - deltaY}px`;
+            e.target.style.left = `${e.clientX - deltaX}px`;
+        }
+    })
+}
+
+let addDivButton = homeworkContainer.querySelector('#addDiv');
+
+addDivButton.addEventListener('click', function() {
+    // создать новый div
+    const div = createDiv();
+
+    // добавить на страницу
+    homeworkContainer.appendChild(div);
+    // назначить обработчики событий мыши для реализации D&D
+    addListeners(div);
+    // можно не назначать обработчики событий каждому div в отдельности, а использовать делегирование
+    // или использовать HTML5 D&D - https://www.html5rocks.com/ru/tutorials/dnd/basics/
+});
+
+export {
+    createDiv
+};
---
+++ src/index.js
@@ -0,0 +1,97 @@
+/* ДЗ 5 - DOM Events */
+
+/*
+ Задание 1:
+
+ Функция должна добавлять обработчик fn события eventName к элементу target
+
+ Пример:
+   addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
+ */
+function addListener(eventName, target, fn) {
+    target.addEventListener(eventName, fn);
+}
+
+/*
+ Задание 2:
+
+ Функция должна удалять у элемента target обработчик fn события eventName
+
+ Пример:
+   removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
+ */
+function removeListener(eventName, target, fn) {
+    target.removeEventListener(eventName, fn);
+}
+
+/*
+ Задание 3:
+
+ Функция должна добавить к элементу target такой обработчик на события eventName, чтобы он отменял действия по умолчанию
+
+ Пример:
+   skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
+ */
+function skipDefault(eventName, target) {
+    target.addEventListener(eventName, e => {
+        event.preventDefault();
+    });
+}
+
+/*
+ Задание 4:
+
+ Функция должна эмулировать событие click для элемента target
+
+ Пример:
+   emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
+ */
+function emulateClick(target) {
+    const eventClick = new Event('click');
+
+    return target.dispatchEvent(eventClick);
+}
+
+/*
+ Задание 5:
+
+ Функция должна добавить такой обработчик кликов к элементу target,
+ который реагирует (вызывает fn) только на клики по элементам BUTTON внутри target
+
+ Пример:
+   delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
+ */
+function delegate(target, fn) {
+    target.addEventListener('click', e => {
+        if (e.target.tagName === 'BUTTON') {
+            fn();
+        }
+    });
+}
+
+/*
+ Задание 6:
+
+ Функция должна добавить такой обработчик кликов к элементу target,
+ который сработает только один раз и удалится (перестанет срабатывать для последующих кликов по указанному элементу)
+
+ Пример:
+   once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
+ */
+function once(target, fn) {
+    function oneClick() {
+        target.removeEventListener('click', oneClick);
+        fn();
+    } 
+
+    target.addEventListener('click', oneClick);
+}
+
+export {
+    addListener,
+    removeListener,
+    skipDefault,
+    emulateClick,
+    delegate,
+    once
+};
