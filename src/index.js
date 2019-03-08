b7cc3a8d7bcef929c4122a1b923ea282542e1fbb
andmmm <andmmm@mail.ru>
2019-03-08 19:19:53 +0400

    homework-6.2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@
 
 /*
  Задание 1:
-
  Функция должна возвращать Promise, который должен быть разрешен через указанное количество секунду
-
  Пример:
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
@@ -17,14 +15,10 @@ function delayPromise(seconds) {
 
 /*
  Задание 2:
-
  2.1: Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
-
  Массив городов пожно получить отправив асинхронный запрос по адресу
  https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
-
  2.2: Элементы полученного массива должны быть отсортированы по имени города
-
  Пример:
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
@@ -53,7 +47,7 @@ function loadAndSortTowns() {
         reject();
       } else {
         let towns = xhr.response.sort(compareCity);
-
+        
         resolve(towns);
       }
     });
